f0e4f030d339aa21f49c42f6c56bbf73c1fcc0b1
Takaaki KITANO <kitanow.takaaki@gmail.com>
2018-11-11 01:27:10 +0900

    Update nuxt

--- modules/typescript.js
+++ modules/typescript.js
@@ -18,12 +18,6 @@ module.exports = function() {
         tsLoader
       )
     )
-    // Add TypeScript loader for vue files
-    for (let rule of config.module.rules) {
-      if (rule.loader === "vue-loader") {
-        rule.options.loaders.ts = tsLoader
-      }
-    }
     // Add .ts extension in webpack resolve
     if (
       config.resolve.extensions.indexOf(".ts") ===
